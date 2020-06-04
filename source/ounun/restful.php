bf33585ad2ac832c21119bd7359d657e9d2cbe3e
yiping <dreamxyp@gmail.com>
2020-06-04 15:25:31 +0800

    no message

--- source/ounun/restful.php
+++ source/ounun/restful.php
@@ -23,9 +23,6 @@ class restful
     /** @var string accept */
     protected $_http_accept;
 
-    /** @var string http_version */
-    protected $_http_version = 'HTTP/1.1';
-
     /** @var string 插件标识 */
     protected $_addon_tag = '';
 
@@ -74,7 +71,7 @@ class restful
      * @param string $methods
      * @param string $domain
      */
-    static public function headers_allow_origin_set(string $methods = 'GET,POST,PUT,DELETE,OPTIONS', string $domain = '*', string $headers = '*')
+    static public function headers_allow_origin_set(string $methods = 'GET,POST,PUT,DELETE', string $domain = '*', string $headers = '*')
     {
         header('Access-Control-Allow-Credentials: true');
         header('Access-Control-Allow-Origin: ' . $domain);
@@ -89,55 +86,16 @@ class restful
      */
     public static function headers_set(string $content_type, int $status_code = 200, string $http_version = 'HTTP/1.1')
     {
-        $Http_Status_Message = [
-            100 => 'Continue',
-            101 => 'Switching Protocols',
-            200 => 'OK',
-            201 => 'Created',
-            202 => 'Accepted',
-            203 => 'Non-Authoritative Information',
-            204 => '资源有空表示(No Content)',
-            205 => 'Reset Content',
-            206 => 'Partial Content',
-            300 => 'Multiple Choices',
-            301 => '资源的URI已被更新(Moved Permanently)',
-            302 => 'Found',
-            303 => '其他（如，负载均衡）(See Other)',
-            304 => '资源未更改（缓存）(Not Modified)',
-            305 => 'Use Proxy',
-            306 => '(Unused)',
-            307 => 'Temporary Redirect',
-            400 => '指代坏请求(Bad Request)',
-            401 => 'Unauthorized',
-            402 => 'Payment Required',
-            403 => 'Forbidden',
-            404 => '资源不存在(Not Found)',
-            405 => 'Method Not Allowed',
-            406 => '服务端不支持所需表示(Not Acceptable)',
-            407 => 'Proxy Authentication Required',
-            408 => 'Request Timeout',
-            409 => '通用冲突(Conflict)',
-            410 => 'Gone',
-            411 => 'Length Required',
-            412 => 'Precondition Failed',
-            413 => 'Request Entity Too Large',
-            414 => 'Request-URI Too Long',
-            415 => 'Unsupported Media Type',
-            416 => 'Requested Range Not Satisfiable',
-            417 => 'Expectation Failed',
-            500 => '通用错误响应(Internal Server Error)',
-            501 => 'Not Implemented',
-            502 => 'Bad Gateway',
-            503 => '服务端当前无法处理请求(Service Unavailable)',
-            504 => 'Gateway Timeout',
-            505 => 'HTTP Version Not Supported'
-        ];
-        $status_message      = $Http_Status_Message[$status_code] ?? $Http_Status_Message[200];
+        $status_message = \ounun\restful\error_code::Maps[$status_code] ?? \ounun\restful\error_code::Maps[200];
 
         header($http_version . ' ' . $status_code . ' ' . $status_message);
         header('Content-Type: ' . $content_type . '; charset=utf-8');
     }
 
+    /**
+     * @param string $key
+     * @return array|mixed
+     */
     public function gets_get($key = '')
     {
         if ($key) {
@@ -146,6 +104,10 @@ class restful
         return $this->_request_gets;
     }
 
+    /**
+     * @param string $key
+     * @return array|mixed
+     */
     public function post_get($key = '')
     {
         if ($key) {
@@ -154,6 +116,10 @@ class restful
         return $this->_request_post;
     }
 
+    /**
+     * @param string $key
+     * @return array|mixed
+     */
     public function input_get($key = '')
     {
         if ($key) {
@@ -162,72 +128,11 @@ class restful
         return $this->_request_inputs;
     }
 
+    /**
+     * @return string
+     */
     public function method_get()
     {
         return $this->_method;
     }
-
-//    /**
-//     * @param mixed $raw_data
-//     * @param int $status_code
-//     * @param string $request_content_type
-//     */
-//    public function out($raw_data, int $status_code = 200, string $request_content_type = '')
-//    {
-//        $request_content_type = $request_content_type ?? $this->_http_accept;
-//        static::headers_set($request_content_type, $status_code, $this->_http_version);
-//
-//        if (strpos($request_content_type, 'application/json') !== false) {
-//            $response = $this->encode_json($raw_data);
-//        } else if (strpos($request_content_type, 'text/html') !== false) {
-//            $response = $this->encode_html($raw_data);
-//        } else if (strpos($request_content_type, 'application/xml') !== false) {
-//            $response = $this->encode_xml($raw_data);
-//        } else {
-//            $response = $this->encode_json($raw_data);
-//        }
-//        exit($response);
-//    }
-
-
-//    /**
-//     * @param $response_data
-//     * @return string
-//     */
-//    public function encode_html($response_data)
-//    {
-//        if (is_array($response_data)) {
-//            $html_response = '<table style="border: darkcyan solid 1px;">';
-//            foreach ($response_data as $key => $value) {
-//                $html_response .= "<tr><td>" . $key . "</td><td>" . $value . "</td></tr>";
-//            }
-//            $html_response .= "</table>";
-//            return $html_response;
-//        }
-//        return $response_data;
-//    }
-
-//    /**
-//     * @param $response_data
-//     * @return false|string
-//     */
-//    public function encode_json($response_data)
-//    {
-//        $jsonResponse = json_encode($response_data);
-//        return $jsonResponse;
-//    }
-
-//    /**
-//     * @param $response_data
-//     * @return mixed
-//     */
-//    public function encode_xml($response_data)
-//    {
-//        // 创建 SimpleXMLElement 对象
-/*        $xml = new \SimpleXMLElement('<?xml version="1.0"?><site></site>');*/
-//        foreach ($response_data as $key => $value) {
-//            $xml->addChild($key, $value);
-//        }
-//        return $xml->asXML();
-//    }
 }
--- source/ounun/restful/error_code.php
+++ source/ounun/restful/error_code.php
@@ -9,5 +9,71 @@ namespace ounun\restful;
 
 class error_code
 {
-
+    /**
+     * GET:
+     *   安全且幂等
+     *     获取表示
+     *     变更时获取表示（缓存）
+     *
+     * POST:
+     *    不安全且不幂等
+     *      使用服务端管理的（自动产生）的实例号创建资源
+     *      创建子资源
+     *      部分更新资源
+     *      如果没有被修改，则不过更新资源（乐观锁）
+     * PUT:
+     *    不安全但幂等
+     *      用客户端管理的实例号创建一个资源
+     *      通过替换的方式更新资源
+     *      如果未被修改，则更新资源（乐观锁）
+     *
+     * DELETE
+     *     不安全但幂等
+     *        删除资源
+     *
+     * @var string[]
+     */
+    const Maps = [
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        200 => 'OK',          // GET:表示已在响应中发出 POST:如果现有资源已被更改  PUT:如果已存在资源被更改 DELETE:资源已被删除
+        201 => 'Created',     //                     POST:如果新资源被创建     PUT:如果新资源被创建
+        202 => 'Accepted',    //                     POST:已接受处理请求但尚未完成（异步处理）
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',  // GET:资源有空表示
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',// GET|POST|PUT|DELETE:资源的URI已被更新
+        302 => 'Found',
+        303 => 'See Other',        // GET|POST|PUT|DELETE:其他（如，负载均衡）
+        304 => 'Not Modified',     // GET|POST:资源未更改（缓存）
+        305 => 'Use Proxy',
+        306 => 'Unused',
+        307 => 'Temporary Redirect',
+        400 => 'Bad Request',      // GET|POST|PUT|DELETE:指代坏请求（如，参数错误）
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',        // GET|POST|PUT|DELETE:资源不存在
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',   // GET|POST|PUT:服务端不支持所需表示
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',         // GET|PUT|DELETE:通用冲突
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',     // POST|PUT:前置条件失败（如执行条件更新时的冲突）
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',  // POST|PUT:接受到的表示不受支持
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        500 => 'Internal Server Error',   // GET|POST|PUT|DELETE:通用错误响应
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',     // GET|POST|PUT|DELETE:服务端当前无法处理请求
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported'
+    ];
 }
